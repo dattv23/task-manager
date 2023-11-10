0f28868c3283462b8544f1197d59e8471667a15a
dattv23 <datvan635@gmail.com>
2023-11-10 20:02:48 +0700

    refactory: add try catch for controller

--- back-end/src/controller/userController.ts
+++ back-end/src/controller/userController.ts
@@ -7,81 +7,106 @@ import sendOTP from '../utils/sendOTP';
 import generateTokens from '../utils/generateToken';
 
 const register = async (req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
+      try {
+            const errors = validationResult(req);
 
-      if (!errors.isEmpty()) {
-            res.status(422).json({ errors: errors.array() });
-            return;
+            if (!errors.isEmpty()) {
+                  res.status(422).json({ errors: errors.array() });
+                  return;
+            }
+
+            const hashPassword = CryptoJS.HmacSHA256(req.body.password, `${process.env.SECRET_KEY}`);
+            await new User({ ...req.body, password: hashPassword }).save();
+
+            sendOTP(req.body.email);
+
+            res.status(201).json({ error: false, message: "Account created sucessfully" });
+      } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: true, message: "Internal Server Error" });
       }
-
-      const hashPassword = CryptoJS.HmacSHA256(req.body.password, `${process.env.SECRET_KEY}`);
-      await new User({ ...req.body, password: hashPassword }).save();
-
-      sendOTP(req.body.email);
-
-      res.status(201).json({ error: false, message: "Account created sucessfully" });
 };
 
 const verifyUser = async (req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
+      try {
+            const errors = validationResult(req);
 
-      if (!errors.isEmpty()) {
-            res.status(422).json({ errors: errors.array() });
-            return;
-      }
+            if (!errors.isEmpty()) {
+                  res.status(422).json({ errors: errors.array() });
+                  return;
+            }
 
-      const user = await User.findOneAndUpdate({ email: req.body.email }, { verify: true });
-      if (user) {
-            const { accessToken, refreshToken } = await generateTokens(user.id, user.roles);
-            res.cookie("access_token", accessToken);
-            res.cookie("refresh_token", refreshToken);
-            res.status(200).json({
-                  error: false, message: "Verify user sucessfully", accessToken, refreshToken
-            });
+            const user = await User.findOneAndUpdate({ email: req.body.email }, { verify: true });
+            if (user) {
+                  const { accessToken, refreshToken } = await generateTokens(user.id, user.roles);
+                  res.cookie("access_token", accessToken);
+                  res.cookie("refresh_token", refreshToken);
+                  res.status(200).json({
+                        error: false, message: "Verify user sucessfully", accessToken, refreshToken
+                  });
+            }
+      } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: true, message: "Internal Server Error" });
       }
 }
 
 const resendVerifyUser = async (req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
+      try {
+            const errors = validationResult(req);
 
-      if (!errors.isEmpty()) {
-            res.status(422).json({ errors: errors.array() });
-            return;
+            if (!errors.isEmpty()) {
+                  res.status(422).json({ errors: errors.array() });
+                  return;
+            }
+
+            sendOTP(req.body.email);
+
+            res.status(201).json({ error: false, message: "Send otp sucessfully" });
+      } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: true, message: "Internal Server Error" });
       }
-
-      sendOTP(req.body.email);
-
-      res.status(201).json({ error: false, message: "Send otp sucessfully" });
 }
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
+      try {
+            const errors = validationResult(req);
 
-      if (!errors.isEmpty()) {
-            res.status(422).json({ errors: errors.array() });
-            return;
-      }
+            if (!errors.isEmpty()) {
+                  res.status(422).json({ errors: errors.array() });
+                  return;
+            }
 
-      const user = await User.findOne({ email: req.body.email, password: CryptoJS.HmacSHA256(req.body.password, `${process.env.SECRET_KEY}`) }, { verify: true });
-      if (user) {
-            const { accessToken, refreshToken } = await generateTokens(user.id, user.roles);
-            res.cookie("access_token", accessToken);
-            res.cookie("refresh_token", refreshToken);
-            res.status(200).json({
-                  error: false, message: "Login sucessfully", accessToken, refreshToken
-            });
-      } else {
-            res.status(200).json({
-                  error: true, message: "Email or password not incorected!"
-            });
+            const user = await User.findOne({ email: req.body.email, password: CryptoJS.HmacSHA256(req.body.password, `${process.env.SECRET_KEY}`) }, { verify: true });
+            if (user) {
+                  const { accessToken, refreshToken } = await generateTokens(user.id, user.roles);
+                  res.cookie("access_token", accessToken);
+                  res.cookie("refresh_token", refreshToken);
+                  res.status(200).json({
+                        error: false, message: "Login sucessfully", accessToken, refreshToken
+                  });
+            } else {
+                  res.status(200).json({
+                        error: true, message: "Email or password not incorected!"
+                  });
+            }
+      } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: true, message: "Internal Server Error" });
       }
 };
 
 const getDetailUser = async (req: Request, res: Response, next: NextFunction) => {
       // console.log(req.body.user);
-      const user = await User.findById(req.body.user._id);
+      try {
+            const user = await User.findById(req.body.user._id);
 
-      res.status(200).json({ message: "user authenticated.", user });
+            res.status(200).json({ message: "user authenticated.", user });
+      } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: true, message: "Internal Server Error" });
+      }
 }
 
 export default { register, verifyUser, resendVerifyUser, login, getDetailUser };
