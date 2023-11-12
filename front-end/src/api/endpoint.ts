8211f2f5ae483c6d6e10f1130f0c227b894ab0c6
dattv23 <datvan635@gmail.com>
2023-11-12 15:48:14 +0700

    feat: setup axios

---
+++ front-end/src/api/endpoint.ts
@@ -0,0 +1,9 @@
+export const BASE_URL = "http://127.0.0.1:8000/api/";
+
+export const LOGIN = BASE_URL + 'login';
+export const REGISTER = BASE_URL + 'register';
+export const VERIFY_USER = BASE_URL + 'verify-user';
+export const RESEND_VERIFY_USER = BASE_URL + 'resend-verify-user';
+export const DETAIL = BASE_URL + 'details';
+export const REFRESH_TOKEN = BASE_URL + 'refresh-token';
+export const LOGOUT = BASE_URL + 'logout';     
