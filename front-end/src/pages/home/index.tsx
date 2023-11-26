3ef5dcb0ddb807141b1d1b168373b65e4afe1016
dattv23 <datvan635@gmail.com>
2023-11-26 12:27:58 +0700

    refactor: page home

--- front-end/src/pages/home/index.tsx
+++ front-end/src/pages/home/index.tsx
@@ -1,34 +1,66 @@
-import { Link } from 'react-router-dom'
-import { Button } from '../../components/Button'
-import { IMAGES } from '../../assets/images/images'
+import { Link } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import { IMAGES } from '../../assets/images/images';
 
 export default function Home() {
-  return (
-    <div className="container mx-auto relative h-screen">
-      <div className="flex justify-between px-8">
-        <Link to={'/'} className="text-primary text-[48px] font-semibold font-['Poppins']">Alliance.</Link>
-        <div className="flex justify-end items-center gap-2">
-          <Button to="/register" variant={'primary'} size={'sm'} className="w-28">Sign Up</Button>
-          <Button to="/login" variant={'secondary'} size={'sm'} className="w-28">Sign In</Button>
-        </div>
-      </div>
-      <div className="px-8 grid lg:grid-cols-2 md:grid-cols-1 md:gap-2 relative">
-        <div className="mt-36 flex flex-col gap-2">
-          <p className="text-5xl font-semibold font-['Poppins']">
-            Task Management &<br />To-Do List
-          </p>
-          <p className="text-zinc-500 text-[24px] font-normal font-['Poppins']">This productive tool is designed to help<br />you better manage your task project-wise <br />conveniently!</p>
-          <Button to="/tasks" className="w-full max-w-[448px] mt-4 text-2xl font-semibold">Let’s Start</Button>
-        </div>
-        <div className="z-10">
-          <img src={IMAGES.female} className="absolute top-40 left-[780px] w-[448px]" />
-          <img src={IMAGES.calendar} className="absolute top-36 right-24" />
-          <img src={IMAGES.stopwatch} className="absolute left-[720px] top-10 h-[140px]" />
-          <img src={IMAGES.pie_chart} className="absolute left-[680px] top-64 h-16" />
-          <img src={IMAGES.notifications} className="absolute left-[1120px] top-80" />
-        </div>
-      </div>
-      <img src={IMAGES.vector} className="absolute bottom-0 lg:h-40 z-0 w-[1200px]" />
-    </div >
-  )
+	return (
+		<div className='container mx-auto relative h-screen'>
+			<div className='flex px-8 flex-col items-center 2xl:flex-row 2xl:justify-between'>
+				<Link
+					to={'/'}
+					className="text-primary text-[48px] font-semibold font-['Poppins']"
+				>
+					Alliance.
+				</Link>
+				<div className='flex justify-end items-center gap-2'>
+					<Button
+						to='/register'
+						variant={'primary'}
+						size={'sm'}
+						className='w-28'
+					>
+						Sign Up
+					</Button>
+					<Button
+						to='/login'
+						variant={'secondary'}
+						size={'sm'}
+						className='w-28'
+					>
+						Sign In
+					</Button>
+				</div>
+			</div>
+			<div className='px-8 grid lg:grid-cols-2 md:grid-cols-1 md:gap-2 mt-14'>
+				<div className='mt-36 flex flex-col gap-2'>
+					<p className="text-5xl font-semibold font-['Poppins']">
+						Task Management &<br />
+						To-Do List
+					</p>
+					<p className="text-zinc-500 text-[24px] font-normal font-['Poppins']">
+						This productive tool is designed to help
+						<br />
+						you better manage your task project-wise <br />
+						conveniently!
+					</p>
+					<Button
+						to='/tasks'
+						className='w-full max-w-[448px] mt-4 text-2xl font-semibold'
+					>
+						Let’s Start
+					</Button>
+				</div>
+				<div className='relative w-0 2xl:w-full'>
+					<img src={IMAGES.female} className='absolute top-40 left-32' />
+					<img src={IMAGES.calendar} className='absolute top-32 right-2' />
+					<img src={IMAGES.stopwatch} className='absolute top-4 left-8' />
+					<img src={IMAGES.pie_chart} className='absolute top-72' />
+					<img
+						src={IMAGES.notifications}
+						className='absolute top-[410px] left-[520px]'
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,31 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#3754DB",
-        accenct_dark: "#16171D",
-        accenct_wine: "#FCCD65",
-        error: "#FB151A",
-        warning: "#EBA300",
-        info: "#6684FF",
-        success: "#00C271"
+        primary: '#3754DB',
+        accent_dark: '#16171D',
+        accent_wine: '#FCCD65',
+        error: '#FB151A',
+        warning: '#EBA300',
+        info: '#6684FF',
+        success: '#00C271',
+        gray: '#62667E'
       },
       screens: {
+        'mobile': '480px',
+        // => @media (min-width: 480px) { ... }
+
         'tablet': '640px',
         // => @media (min-width: 640px) { ... }
 
         'laptop': '1024px',
         // => @media (min-width: 1024px) { ... }
 
-        'desktop': '1280px',
+        'desktop': '1280px'
         // => @media (min-width: 1280px) { ... }
       },
-    },
+      fontFamily: {
+        'gelion': 'Gelion'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
