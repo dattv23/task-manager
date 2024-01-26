c5b08a9c4efc9fb5c7990112086fd087f77f2444
dattv23 <datvan635@gmail.com>
2024-01-26 21:57:21 +0700

    feat: component alert

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
-import { cn } from '~/utils'
+import { useState } from 'react'
+import Alert from './components/Alert'
 
 const App = () => {
+  const [alertOpen, setAlertOpen] = useState(false)
+
+  const handleAlertClose = () => {
+    setAlertOpen(false)
+  }
+
+  const handleShowAlert = () => {
+    setAlertOpen(true)
+  }
+
   return (
     <div>
-      <h1 className={cn('font-bold', 'text-red-200', 1 > 2 && 'border')}>Hello world!</h1>
+      <button onClick={handleShowAlert}>Show Alert</button>
+
+      <Alert
+        open={alertOpen}
+        content='This is a custom alert created using React and TypeScript.'
+        onClose={handleAlertClose}
+        type={'info'}
+      />
     </div>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    colors: {
+      primary: '#3754DB',
+      secondary: '#FCCD65',
+      error: '#FB151A',
+      warning: '#EBA300',
+      info: '#6684FF',
+      success: '#00C271',
+      dark: '#16171D',
+      white: '#ffff',
+      wine: '#B80020'
+    },
+    extend: {
+      fontFamily: {
+        popins: ['Poppins', 'sans-serif'],
+        smooch: ['Smooch Sans', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
