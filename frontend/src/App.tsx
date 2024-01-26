055356444c37df8efbd681970d12770edb0148e5
dattv23 <datvan635@gmail.com>
2024-01-26 14:09:10 +0700

    feat: merge class name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import { cn } from '~/utils'
+
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1 className={cn('font-bold', 'text-red-200', 1 > 2 && 'border')}>Hello world!</h1>
     </div>
   )
 }
