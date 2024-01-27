a639e69575ecde46fdae947762dae4ad33d5b329
dattv23 <datvan635@gmail.com>
2024-01-27 18:58:01 +0700

    feat: button component

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,18 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      primary: '#3754DB',
-      secondary: '#FCCD65',
-      error: '#FB151A',
-      warning: '#EBA300',
-      info: '#6684FF',
-      success: '#00C271',
-      dark: '#16171D',
-      white: '#ffff',
-      wine: '#B80020'
-    },
     extend: {
+      colors: {
+        primary: '#3754DB',
+        secondary: '#FCCD65',
+        error: '#FB151A',
+        warning: '#EBA300',
+        info: '#6684FF',
+        success: '#00C271',
+        dark: '#16171D',
+        white: '#ffff',
+        wine: '#B80020'
+      },
       fontFamily: {
         popins: ['Poppins', 'sans-serif'],
         smooch: ['Smooch Sans', 'sans-serif']
