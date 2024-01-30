75f249587dae267c29a08681b93b89b4ff27d870
dattv23 <datvan635@gmail.com>
2024-01-30 21:00:38 +0700

    feat: add error boundary component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Root from '~/components/Root/Root'
+import ErrorBoundary from '~/components/ErrorBoundary'
 
-const router = createBrowserRouter([{ path: '*', Component: Root }])
+const router = createBrowserRouter([{ path: '*', Component: Root, errorElement: <ErrorBoundary /> }])
 
 const App = () => {
   return <RouterProvider router={router} />
