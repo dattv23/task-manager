50b74dec8a1fc82eae174ffc090e56e53942d331
dattv23 <datvan635@gmail.com>
2024-01-31 14:22:56 +0700

    refactor: form item component

--- frontend/src/components/FormItem/FormItem.tsx
+++ frontend/src/components/FormItem/FormItem.tsx
@@ -1,20 +1,31 @@
 import { Form, Input } from 'antd'
 import { Rule } from 'antd/es/form'
+import { ReactNode } from 'react'
 
 interface FormItemProps {
-  name: string
-  label: string
+  name?: string
+  label?: string
   placeholder?: string
-  rules: Rule[]
+  rules?: Rule[]
   type?: 'text' | 'password'
+  children?: ReactNode
 }
 
-const FormItem: React.FC<FormItemProps> = ({ name, label = '', placeholder = '', rules, type = 'text' }) => {
+const FormItem: React.FC<FormItemProps> = ({
+  name = '',
+  label = '',
+  placeholder = '',
+  rules,
+  type = 'text',
+  children
+}) => {
   return (
     <>
       <p className='mb-3 text-base font-semibold text-gray-800'>{label}</p>
       <Form.Item name={name} rules={rules}>
-        {type === 'text' ? (
+        {children ? (
+          children
+        ) : type === 'text' ? (
           <Input
             className='h-12 w-full border-[2px] border-primary border-opacity-80 py-2 text-base font-normal focus:border-opacity-100'
             classNames={{ input: 'text-md font-normal font-popins' }}
