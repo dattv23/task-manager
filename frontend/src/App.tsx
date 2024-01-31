3483b8370392ea994b6716b17c8de5f1f8c7a0df
dattv23 <datvan635@gmail.com>
2024-01-31 14:23:35 +0700

    feat: suspense loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Root from '~/components/Root/Root'
 import ErrorBoundary from '~/components/ErrorBoundary'
+import { Suspense } from 'react'
+import { Spin } from 'antd'
 
 const router = createBrowserRouter([{ path: '*', Component: Root, errorElement: <ErrorBoundary /> }])
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <Suspense fallback={<Spin size='large' tip='Loading...' />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  )
 }
 
 export default App
