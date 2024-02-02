8d3b58cc5c5203dcb689b18ef4a3cf1e97e3fc42
dattv23 <datvan635@gmail.com>
2024-02-02 14:03:54 +0700

    feat: ui home

--- frontend/src/assets/images/index.ts
+++ frontend/src/assets/images/index.ts
@@ -1,5 +1,7 @@
-import vector from './Vector.png'
+import description from './description.png'
+import vector from './vector.png'
 
 export const IMAGES = {
+  description: description,
   vector: vector
 }
