6e97e2fec96f491cf6854e9d03a03ec2660c52ea
dattv23 <datvan635@gmail.com>
2024-02-18 10:07:18 +0700

    add: images and icons

---
+++ frontend/src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+import onboarding from './onboarding.svg'
+import workspace from './workspace.svg'
+
+export const ICONS = {
+  onboarding: onboarding,
+  workspace: workspace
+}
--- frontend/src/assets/images/index.ts
+++ frontend/src/assets/images/index.ts
@@ -1,9 +1,11 @@
 import description from './description.png'
 import vector from './vector.png'
 import avatar from './avatar.jpg'
+import overview from './overview.png'
 
 export const IMAGES = {
   description: description,
   vector: vector,
-  avatar: avatar
+  avatar: avatar,
+  overview: overview
 }
