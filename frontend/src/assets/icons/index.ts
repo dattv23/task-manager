b76c46de14538421ec481a4dc5c2d880292b11e4
dattv23 <datvan635@gmail.com>
2024-02-18 10:37:49 +0700

    feat: ui overview dashboard

--- frontend/src/assets/icons/index.ts
+++ frontend/src/assets/icons/index.ts
@@ -1,7 +1,9 @@
 import onboarding from './onboarding.svg'
 import workspace from './workspace.svg'
+import arrowRight from './arrow-right.svg'
 
 export const ICONS = {
   onboarding: onboarding,
-  workspace: workspace
+  workspace: workspace,
+  arrowRight: arrowRight
 }
