132ff39d8b4e6f32e9ab4766b3756cae2bf94036
dattv23 <datvan635@gmail.com>
2024-02-20 15:47:33 +0700

    feat: remaining time when user reload page

--- frontend/src/container/Auth/components/Verification/Verification.tsx
+++ frontend/src/container/Auth/components/Verification/Verification.tsx
@@ -1,9 +1,9 @@
-import { Form, Input } from 'antd'
+import { CountdownProps, Form, Input } from 'antd'
 import { VerificationField } from '~/@types/form.type'
-import { useResendOTPMutation, useVerifyOTPMutation } from '~/apis/api'
+import { APIErrorResult, useResendOTPMutation, useVerifyOTPMutation } from '~/apis/api'
 import { Button, FormItem } from '~/components'
 import { useToasts } from '~/hooks/useToasts'
-import { clearStore, mergeValues } from '~/utils'
+import { clearStore, getStore, mergeValues, setStore } from '~/utils'
 import { Statistic } from 'antd'
 import { useEffect, useState } from 'react'
 const { Countdown } = Statistic
@@ -19,33 +19,61 @@ const Verification: React.FC<VerificationProps> = ({ email, next }) => {
   const { addToast } = useToasts()
   const [deadline, setDeadline] = useState<number>(Date.now() + 1000 * 60 * 5)
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    const remainingTime = getStore('remainingTime')
+    if (remainingTime) {
+      setDeadline(Date.now() + parseInt(remainingTime))
+    } else {
+      setDeadline(Date.now() + 1000 * 60 * 5)
+    }
+  }, [])
 
   const handleSubmit = async (values: VerificationField) => {
     const code = mergeValues(values)
     const res = await verifyOTP({ email, code })
     if ('data' in res) {
+      addToast({
+        title: 'Verification successful',
+        message: 'Please try login again!',
+        type: 'success',
+        progress: true,
+        timeOut: 5
+      })
       clearStore('remainingTime')
       next()
     }
     if ('error' in res) {
-      if ('status' in res.error) {
-        if (res.error.status === 404 && 'data' in res.error) {
-          addToast({
-            title: 'Warning',
-            message: res.error.data.message,
-            type: 'warning'
-          })
-        }
+      const { status, data } = res.error as APIErrorResult
+      if (!status) {
+        addToast({
+          title: 'Verification failed',
+          message: 'Please try again later!',
+          type: 'error',
+          progress: true,
+          timeOut: 5
+        })
+        return
+      }
+      if (typeof data === 'string') {
+        addToast({
+          title: 'Verification failed',
+          message: data,
+          type: 'error',
+          progress: true,
+          timeOut: 5
+        })
       } else {
         addToast({
-          title: 'Error',
-          message: 'Please try again later!',
-          type: 'error'
+          title: 'Verification failed',
+          message: data.message,
+          type: 'error',
+          progress: true,
+          timeOut: 5
         })
       }
     }
   }
+
   const handleResendOTP = async () => {
     const res = await resendOTP({ email })
     if ('data' in res) {
@@ -69,6 +97,12 @@ const Verification: React.FC<VerificationProps> = ({ email, next }) => {
     }
   }
 
+  const onChange: CountdownProps['onChange'] = (val) => {
+    if (typeof val === 'number') {
+      setStore('remainingTime', val)
+    }
+  }
+
   return (
     <div className='text-center'>
       <h3 className='mb-3 text-2xl font-semibold text-black'>Verification Email</h3>
@@ -76,7 +110,7 @@ const Verification: React.FC<VerificationProps> = ({ email, next }) => {
         Please enter the OTP sent to your registered email address to complete the verification process.
       </p>
       <div className='mb-4 text-center'>
-        <Countdown value={deadline} format='mm:ss' />
+        <Countdown value={deadline} format='mm:ss' onChange={onChange} />
       </div>
       <Form name='basic' onFinish={handleSubmit}>
         <div className='flex justify-between'>
