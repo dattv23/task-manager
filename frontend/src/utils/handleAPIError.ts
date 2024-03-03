ce9c307c4b69d8e7fde64619f1b43c58ab9f7c26
dattv23 <datvan635@gmail.com>
2024-03-03 15:52:46 +0700

    refactor: change type error and catch error

--- frontend/src/utils/handleAPIError.ts
+++ frontend/src/utils/handleAPIError.ts
@@ -1,6 +1,12 @@
-export const handleAPIError = (error: { status?: number; data: any }) => {
-  const { status, data } = error
-  const { message, errors } = data as { message: string; errors?: [{ path: string; message: string }] }
+export const handleAPIError = (error: any) => {
+  const { status, data } = error as {
+    status?: number
+    data: { message: string; errors?: [{ path: string; message: string }] }
+  }
+  if (!status) {
+    return { title: 'Error', message: 'Please try again later!' }
+  }
+  const { message, errors } = data
   if (status == 422 && errors) {
     return { title: 'Error', message: errors[0].message, status }
   }
