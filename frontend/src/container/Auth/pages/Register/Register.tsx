57f4024ca923a167c7b23a5f5c532be6f4aa7a27
dattv23 <datvan635@gmail.com>
2024-03-03 15:37:10 +0700

    refactor: update registration form with new design and error handling.

--- frontend/src/container/Auth/pages/Register/Register.tsx
+++ frontend/src/container/Auth/pages/Register/Register.tsx
@@ -1,13 +1,16 @@
-import { Form } from 'antd'
+import React from 'react'
+import { Alert, Form, Input } from 'antd'
 import Button from '~/components/Button'
-import FormItem from '~/components/FormItem'
 import { useNavigate } from 'react-router-dom'
-import { emailRegex, passwordRegex } from '~/utils/regex'
+import { passwordRegex } from '~/utils/regex'
 import { RegisterField } from '~/@types/form.type'
-import { APIErrorResult, useRegisterMutation } from '~/apis/api'
+import { useRegisterMutation } from '~/apis/api'
 import { useToasts } from '~/hooks/useToasts'
 import { RegisterResult } from '~/@types/api.type'
 import { setStore } from '~/utils'
+import { handleAPIError } from '~/utils/handleAPIError'
+import { FormItem } from '~/components'
+import { InputProps } from '~/components/FormItem'
 
 const Register: React.FC = () => {
   const navigate = useNavigate()
@@ -23,41 +26,74 @@ const Register: React.FC = () => {
       navigate('/verify-email', { state: { email: email } })
     }
     if ('error' in res) {
-      const { status, data } = res.error as APIErrorResult
-      if (!status) {
-        addToast({
-          title: 'Register failed',
-          message: 'Please try again later!',
-          type: 'error',
-          progress: true,
-          timeOut: 5
-        })
-        return
-      }
-      if (typeof data === 'string') {
-        addToast({
-          title: 'Register failed',
-          message: data,
-          type: 'error',
-          progress: true,
-          timeOut: 5
-        })
-      } else {
-        addToast({
-          title: 'Register failed',
-          message: data.message,
-          type: 'error',
-          progress: true,
-          timeOut: 5
-        })
-      }
+      const { message } = handleAPIError(res.error)
+      addToast({ title: 'Register Failed', message, type: 'error', progress: true, timeOut: 5 })
     }
   }
 
+  const formRegisterInputs: InputProps[] = [
+    {
+      name: 'fullName',
+      label: 'Full Name',
+      placeholder: 'John Doe',
+      rules: [
+        {
+          min: 6,
+          max: 30,
+          message: (
+            <Alert
+              className='bg-transparent text-base text-red-700'
+              message='Full name must be length from 6 to 30 characters.'
+              banner
+              type='error'
+            />
+          )
+        }
+      ]
+    },
+    {
+      name: 'email',
+      label: 'Email Address',
+      placeholder: 'johndoe@example.com',
+      rules: [
+        {
+          type: 'email',
+          message: (
+            <Alert className='bg-transparent text-base text-red-700' message='Email not valid!' banner type='error' />
+          )
+        }
+      ]
+    },
+    {
+      name: 'password',
+      label: 'Password',
+      rules: [
+        {
+          pattern: passwordRegex,
+          message: (
+            <Alert
+              className='bg-transparent text-base text-red-700'
+              message='Over 8 characters and under 36 characters with an Uppercase, symbol and number!'
+              banner
+              type='error'
+            />
+          )
+        }
+      ],
+      children: (
+        <Input.Password
+          placeholder='johnDoe@123'
+          className='h-12 w-full border-[2px] border-primary border-opacity-80 py-2 text-base font-normal focus:border-opacity-100'
+          classNames={{ input: 'text-md font-normal font-popins' }}
+        />
+      )
+    }
+  ]
+
   return (
-    <>
-      <div className='flex h-screen p-5'>
-        <div className='relative hidden items-center justify-center rounded-[32px] bg-primary lg:flex lg:px-[100px] xl:w-1/2'>
+    <div className='fixed bottom-0 left-0 right-0 top-0 overflow-y-auto'>
+      <div className='flex min-h-screen overflow-y-auto p-5'>
+        <div className='relative hidden rounded-[32px] bg-primary lg:flex lg:items-center lg:justify-center lg:px-[100px] xl:w-1/2'>
           <div className='z-10 flex-1'>
             <h2 className='z-20 mb-32 w-[454px] text-[56px] font-semibold leading-[66px] text-white'>
               Take your productivity to the next level.
@@ -70,7 +106,7 @@ const Register: React.FC = () => {
           </div>
           <div className='absolute left-3/4 h-[460px] w-[460px] overflow-hidden rounded-full border-[72px] border-[#FBBE37]'></div>
         </div>
-        <div className='relative  z-10 flex h-full w-full items-center  justify-center bg-white xl:w-1/2'>
+        <div className='z-10 flex w-full items-center justify-center bg-white xl:w-1/2'>
           <Button
             variant={'secondary'}
             onClick={() => navigate('/login')}
@@ -82,39 +118,17 @@ const Register: React.FC = () => {
             <h3 className='text-[28px] font-bold text-black lg:text-[32px]'>Create an Account</h3>
             <p className='mb-2 text-base font-normal text-gray-500'>It's Simple and Easy!!</p>
             <Form name='basic' onFinish={onFinish}>
-              <FormItem
-                name='fullName'
-                label='Full name'
-                placeholder='John Smith'
-                rules={[
-                  { required: true, message: 'Please input your full name!' },
-                  { min: 6, max: 30, message: 'Full name must be length from 6 to 30 characters.' }
-                ]}
-              />
-
-              <FormItem
-                name='email'
-                label='Email Address'
-                placeholder='johndoe@gmail.com'
-                rules={[
-                  { required: true, message: 'Please input your email address!' },
-                  { pattern: emailRegex, message: 'Email not valid!' }
-                ]}
-              />
-
-              <FormItem
-                name='password'
-                label='Password'
-                rules={[
-                  { required: true, message: 'Please input your password!' },
-                  {
-                    pattern: passwordRegex,
-                    message: 'Over 8 characters and under 36 characters with an Uppercase, symbol and number!'
-                  }
-                ]}
-                type='password'
-              />
-
+              {formRegisterInputs.map((item, index) => (
+                <FormItem
+                  key={index}
+                  name={item.name}
+                  label={item.label}
+                  placeholder={item.placeholder}
+                  required={item.required}
+                  rules={item.rules}
+                  children={item.children}
+                />
+              ))}
               <Form.Item>
                 <Button type='submit' className='mt-6 w-full lg:mt-12'>
                   {isLoading ? 'Creating...' : 'Create Account'}
@@ -124,7 +138,7 @@ const Register: React.FC = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
