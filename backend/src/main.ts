a14de05e5304d332862664542cfed0e0e31ad553
dattv23 <datvan635@gmail.com>
2024-08-06 13:51:27 +0700

    add: docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,19 @@
-import express, { NextFunction, Request, Response } from 'express'
-import { sendResponse } from '~/config/response.config'
-import { createServer } from 'http'
 import cors from 'cors'
 import morgan from 'morgan'
+import 'express-async-errors'
+import { createServer } from 'http'
+import session from 'express-session'
 import compression from 'compression'
+import exitHook from 'async-exit-hook'
 import cookieParser from 'cookie-parser'
+import express, { NextFunction, Request, Response } from 'express'
+
 import rootRouter from './routes'
 import { env } from './config/env.config'
+import { sendResponse } from './config/response.config'
+import { DATABASE_MESSAGE } from './constants/messages'
 import { databaseService } from './services/database.services'
 import { errorHandler } from './middlewares/error.middlewares'
-import exitHook from 'async-exit-hook'
-import { DATABASE_MESSAGE } from './constants/messages'
-import 'express-async-errors'
-import session from 'express-session'
-import { Client, generators, Issuer } from 'openid-client'
 
 const app = express()
 const httpServer = createServer(app)
@@ -67,42 +67,6 @@ app.use(
 
 app.use('/api', rootRouter)
 
-// let client: Client
-
-// Initialize OpenID client
-// async function initOpenIDClient() {
-//   const googleIssuer = await Issuer.discover('https://accounts.google.com')
-//   client = new googleIssuer.Client({
-//     client_id: env.auth.client_id!,
-//     client_secret: env.auth.client_secret,
-//     redirect_uris: ['http://localhost:8080/callback'],
-//     response_types: ['code']
-//   })
-// }
-
-// Authentication URL
-// app.get('/auth', (req, res) => {
-//   const code_verifier = generators.codeVerifier()
-//   const code_challenge = generators.codeChallenge(code_verifier)
-//   req.session.code_verifier = code_verifier // Store in session
-
-//   const authUrl = client.authorizationUrl({
-//     scope: 'openid email profile',
-//     code_challenge,
-//     code_challenge_method: 'S256'
-//   })
-//   res.redirect(authUrl)
-// })
-
-// Callback URL
-// app.get('/callback', async (req, res) => {
-//   const params = client.callbackParams(req)
-//   const tokenSet = await client.callback('http://localhost:8080/callback', params, { code_verifier: req.session.code_verifier })
-//   const userinfo = await client.userinfo(tokenSet)
-
-//   res.json({ userinfo })
-// })
-
 app.get('/', (req: Request, res: Response) => {
   sendResponse.success(res, [], 'Hello world')
 })
@@ -114,7 +78,6 @@ databaseService.connect()
 app.use(errorHandler)
 
 httpServer.listen({ port: env.server.port, hostname: env.server.host }, async () => {
-  // await initOpenIDClient()
   console.log(`🚀 Server Is Running At http://${env.server.host}:${env.server.port}`)
 })
 
